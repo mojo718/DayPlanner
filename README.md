bcb29fd69e5619036ecd26a13cc56627eab60c89
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-01-30 16:46:44 -0500

    fixed issue with saving to Local Storage, updated readMe, and changed CSS for nicer colors.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DayPlanner
+
+A simple Day Planner with hourly blocks.  Once a block has been modified it can be saved to local storage but clicking on the blue disc icon.
+
+## Getting Started:
+1. Clone the repository from https://github.com/mojo718/DayPlanner
+2. Open script.js in your favorite code editor to review the Javascript
+
+Live site:
+https://mojo718.github.io/DayPlanner
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,28 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time? **create a currentHour var with dayjs()**
-
-// did some research on this and found https://stackoverflow.com/questions/4676562/jquery-adding-and-removing-class-dynamically
-//https://day.js.org/docs/en/get-set/hour
 $(document).ready(function () {
-  updateTimeBlocks();
-  setInterval(updateTimeBlocks, 60000);
-
+  // Function to update time blocks
   function updateTimeBlocks() {
     var currentHour = dayjs().hour();
 
@@ -38,39 +15,29 @@ $(document).ready(function () {
       }
     });
   }
-});
 
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//still working on this 
+  // Call updateTimeBlocks initially and set interval to update every minute
+  updateTimeBlocks();
+  setInterval(updateTimeBlocks, 60000);
 
-// check to see if anything is local storage and display if there is
-if (typeof (Storage) !== "undefined") {
-  var storedNotes = localStorage.getItem("notes");
-  if (storedNotes !== null) {
-    $("textarea").val(storedNotes);
-  }
+  // Check for and display stored notes
+  $("textarea").each(function () {
+    var row = $(this).attr("data-row");
+    var storedNotes = localStorage.getItem("notes" + row);
+    if (storedNotes !== null) {
+      $(this).val(storedNotes);
+    }
+  });
 
-  // eventhandler on button to record textarea to local storage
-  $("button").on("click", function() {
-    var savedNotes = $("textarea").val();
-    console.log()
-    debugger
-    localStorage.setItem("notes", savedNotes);
-    alert("Appointment saved to local storage");
-  }
-  )
-};
+  // Event handler to save notes to local storage
+  $(".saveBtn").on("click", function () {
+    var row = $(this).attr("data-row");
+    var savedNotes = $(this).siblings(".description").val();
+    localStorage.setItem("notes" + row, savedNotes);
+    alert("Appointment changes saved to local storage");
+  });
 
-//
-// TODO: Add code to display the current date in the header of the page.
-// researched on Jquery.and https://day.js.org/en/
-// added time
-$(document).ready(function () {
+  // Display the current date in the header
   var currentDay = dayjs().format("dddd, MMMM D, YYYY h:mm A");
   $("#currentDay").text(currentDay);
 });
-
-
